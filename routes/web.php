16c2fdfbe7c8bac8ec345cb3f0e7792945be5bc5
monikasmilga <tuuli705@gmail.com>
2017-08-29 15:24:43 +0300

    Grabs data

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/grabbed', ['as' => 'app.data.index', 'uses' => 'GDDataGrabbingController@index']);
+
+Route::get('/result', ['as' => 'app.data.onePageData', 'uses' => 'GDDataGrabbingController@grabAllData']);
